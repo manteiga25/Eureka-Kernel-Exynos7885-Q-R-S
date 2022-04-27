93bd0979f98c435f2bcf5d1df83526a3b619fc88
manteiga25 <alexmykhaylo@gmail.com>
2022-04-27 18:48:00 +0100

    ?

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2080,8 +2080,8 @@ static ssize_t mm_stat_show(struct device *dev,
 			(u64)atomic64_read(&zram->stats.same_pages),
 			pool_stats.pages_compacted,
 			zram_dedup_dup_size(zram),
-			zram_dedup_meta_size(zram));
-			(u64)atomic64_read(&zram->stats.huge_pages);
+			zram_dedup_meta_size(zram),
+			(u64)atomic64_read(&zram->stats.huge_pages));
 	up_read(&zram->init_lock);
 
 	return ret;
